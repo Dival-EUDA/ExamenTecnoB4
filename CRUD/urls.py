9b19d5ce335bdaf36185ba361f33768018c8a629
Divaldo <antifraudeuda@gmail.com>
2021-10-06 22:37:05 -0600

    save6

--- CRUD/urls.py
+++ CRUD/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('', CRUD, name='CRUD'),
+    path('tables/', TABLES, name='tables'),
+    path('formbasic/', FORM, name='form'),
+    path('formwizard/', FORM2, name='formwizard'),
 ]
--- CRUD/views.py
+++ CRUD/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 def CRUD(request):
     return render(request, 'index.html')
 
+def TABLES(request):
+    return render(request, 'tables.html')
+
+def FORM(request):
+    return render(request, 'form-basic.html')
+
+def FORM2(request):
+    return render(request, 'form-wizard.html')
