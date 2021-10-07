4d967a1d7d1cfa4df27e557e8930ed5373f820fc
Divaldo <antifraudeuda@gmail.com>
2021-10-06 21:14:19 -0600

    save4

--- ExamenTecnoB4/settings.py
+++ ExamenTecnoB4/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ofs-qj(o!geg#d((sv2$o!w%urtyazhxb3g15-a4(eg$8uh(0y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
